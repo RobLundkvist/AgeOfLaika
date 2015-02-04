3b023774465a5f6074a2aadcdbbecbb3a490f6c8
Robert Lundkvist <robert@lundkvist.co.uk>
2015-02-04 21:02:05 +0000

    First release

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateHumanYearsToDogYearsButton(sender: UIButton) {
+        var dogYears = humanYearsTextField.text.toInt()! * 7
+        dogYearsLabel.text = "\(dogYears)" + " Dog Years"
+        
+        humanYearsTextField.resignFirstResponder()
+        
+    
+    }
 
 }
 
